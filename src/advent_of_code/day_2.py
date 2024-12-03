c07d29c5cb3df63fc82495782f20162ec3389ff6
Zach Fuller <zach.fuller1222@gmail.com>
2024-12-02 20:17:19 -0700

    this is wrong but at least the output is pretty now

--- src/advent_of_code/day_2.py
+++ src/advent_of_code/day_2.py
@@ -1,8 +1,12 @@
+import contextlib
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal
 
+from rich.columns import Columns
 from rich.console import Console
 from rich.panel import Panel
+from rich.table import Table
 
 console = Console()
 
@@ -87,71 +91,91 @@ def part_1() -> None:
     console.print(Panel(f"[bold green]{safe_report_count=} [/]", title="[bold cyan]Day 2 - Part 1[/]", expand=False))
 
 
-def report_is_safe_part2(report: str) -> bool:  # noqa: C901
-    # TODO: This isn't working because an issue with a single level could trigger violation_count to be incremented twice
-    levels = report.split(" ")
-    violation_count = 0
-    increasing, decreasing = False, False
-    for i in range(len(levels)):
-        curr = int(levels[i])
-        prev, nxt = None, None
-        if i > 0:
-            prev = int(levels[i - 1])
-        if i < len(levels) - 2:
-            nxt = int(levels[i + 1])
-        violation = False
-        if prev:  # If there's an element to the left in the list
-            valid_trend = True
-            if curr < prev:
-                decreasing = True
-                valid_trend = check_trend(increasing, decreasing, "decreasing")
-            elif curr > prev:
-                increasing = True
-                valid_trend = check_trend(increasing, decreasing, "increasing")
+@dataclass
+class Level:
+    prev: int | None
+    curr: int
+    next: int | None
 
-            if not valid_trend:
-                violation = True
-            if not valid_difference(int(prev), int(curr)):
-                violation = True
-        if nxt:
-            valid_trend = True
-            if nxt < curr:
-                decreasing = True
-                valid_trend = check_trend(increasing, decreasing, "decreasing")
-            elif nxt > curr:
-                increasing = True
-                valid_trend = check_trend(increasing, decreasing, "increasing")
 
-            if not valid_trend:
-                violation = True
-            if not valid_difference(int(curr), int(nxt)):
-                violation = True
-        if violation:
-            violation_count += 1
-        if violation_count > 1:
-            return False
+def output_level(levels: list[Level], problem_index: int | None = None) -> None:
+    panels: list[Panel] = []
+    for index, level in enumerate(levels):
+        if problem_index and index == problem_index:
+            panels.append(Panel(f"[bold red]{level.curr}[/]", title=f"[red]{index}[/]", expand=False))
+        else:
+            panels.append(Panel(f"[bold cyan]{level.curr}[/]", title=f"{index}", expand=False))
+    columns = Columns(panels, equal=True, expand=False)
+    console.print(columns)
+
+
+def output_break() -> None:
+    console.print("\n⭐ 🦄 ✨ 🧙 ⭐ 🦄 ✨ 🧙 ⭐ 🦄 ✨ 🧙 ⭐ 🦄 ✨ 🧙\n")
+
+
+def evaluate_report_safety_part2(levels: list[Level]) -> bool:
+    for index, level in enumerate(levels):
+        if level.prev is not None and level.prev < level.curr:
+            # Increasing from prev -> curr
+            if level.next is not None and level.next < level.curr:
+                # Increase -> Decrease
+                raise ValueError(f"Increase->Decrease: {index + 1}")
+            if level.prev == level.curr:
+                raise ValueError(f"SameValue: {index}")
+        elif level.prev is not None and level.prev > level.curr:
+            # Decreasing from prev -> curr
+            if level.next is not None and level.next > level.curr:
+                # Decrease Increase -> Decrease
+                raise ValueError(f"Decrease->Increase: {index}")
+        if level.next is not None:
+            if level.next == level.curr:
+                raise ValueError(f"SameValue: {index + 1}")
+            # Perform difference validation
+            difference = abs(level.next - level.curr)
+            if difference < 1 or difference > 3:
+                raise ValueError(f"InvalidDifference: {index}")
+
     return True
 
 
-def evaluate_report_safety_part2(levels: list[int]) -> bool:
-    return True
-
-
-def tokenize_line(report: str) -> list[int]:
-    return [int(level) for level in report.split(" ")]
+def tokenize_line(report: str) -> list[Level]:
+    level_ints = [int(level) for level in report.split(" ")]
+    levels: list[Level] = []
+    for index, level in enumerate(level_ints):
+        prev, next = None, None
+        if index > 1:
+            prev = level_ints[index - 1]
+        if index < len(level_ints) - 2:
+            next = level_ints[index + 1]
+        levels.append(Level(prev=prev, curr=level, next=next))
+    return levels
 
 
 def calculate_safe_reports_part2(reports: list[str]) -> int:
     count = 0
     for report in reports:
         levels = tokenize_line(report)
-        safe = evaluate_report_safety_part2(levels)
-        if safe:
-            count += 1
-        else:
-            # do crazy stuff to remove an element to see if it passes
-            pass
-
+        try:
+            safe = evaluate_report_safety_part2(levels)
+            if safe:
+                count += 1
+        except ValueError as e:
+            problem_index = int(str(e).split(": ")[-1])
+            output_level(levels, problem_index=problem_index)
+            problem_level = levels[problem_index]
+            levels.remove(problem_level)
+            output_level(levels)
+            safe = False
+            try:
+                safe = evaluate_report_safety_part2(levels)
+                if safe:
+                    console.print(":heavy_check_mark: [bold green]Safe after removing problem child![/]👶")
+                    count += 1
+            except ValueError as e:
+                console.print(f":x: [bold red]Dangerous! {e!s}[/]")
+                problem_index = int(str(e).split(": ")[-1])
+                output_level(levels, problem_index)
+            output_break()
     return count
 
 
