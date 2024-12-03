253ad772b6d8e1ff2d7d098786e7318af4c43ed8
Zach Fuller <zach.fuller1222@gmail.com>
2024-12-02 17:50:36 -0700

    day2 part 1 complete

---
+++ src/advent_of_code/day_2.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+from typing import Literal
+
+from rich.console import Console
+from rich.panel import Panel
+
+console = Console()
+
+
+def read_input() -> list[str]:
+    input_path = Path("input") / "day2.txt"
+    console.print("[bold cyan]Reading input file: [/bold cyan]" + str(input_path))
+    with input_path.open() as f:
+        return f.read().splitlines()
+
+
+def check_trend(increasing: bool, decreasing: bool, trend: Literal["increasing", "decreasing"]) -> bool:
+    if trend == "increasing" and decreasing is True:  # noqa: SIM114
+        return False
+    elif trend == "decreasing" and increasing is True:  # noqa: SIM103
+        return False
+    else:
+        return True
+
+
+def valid_difference(elem_a: int, elem_b: int) -> bool:
+    difference = abs(elem_a - elem_b)
+    return difference >= 1 and difference <= 3
+
+
+def check_rules_curr_next(curr: int, nxt: int, increasing: bool, decreasing: bool) -> bool:
+    if nxt < curr:
+        decreasing = True
+        valid_trend = check_trend(increasing, decreasing, "decreasing")
+    elif nxt > curr:
+        increasing = True
+        valid_trend = check_trend(increasing, decreasing, "increasing")
+    else:
+        return False
+    if not valid_trend:
+        return False
+    # Assume trend is valid at this point, so check difference between current and next
+    return valid_difference(int(curr), int(nxt))
+
+
+def report_is_safe(report: str) -> bool:
+    levels = report.split(" ")
+    increasing, decreasing = False, False
+    for i in range(len(levels)):
+        curr = int(levels[i])
+        prev, nxt = None, None
+        if i > 0:
+            prev = int(levels[i - 1])
+        if i < len(levels) - 2:
+            nxt = int(levels[i + 1])
+
+        if prev:  # If there's an element to the left in the list
+            if curr < prev:
+                decreasing = True
+                valid_trend = check_trend(increasing, decreasing, "decreasing")
+            elif curr > prev:
+                increasing = True
+                valid_trend = check_trend(increasing, decreasing, "increasing")
+            else:
+                return False
+            if not valid_trend:
+                return False
+            # Assume trend is valid at this point, so check difference between prev and current
+            if not valid_difference(int(prev), int(curr)):
+                return False
+        if nxt and check_rules_curr_next(curr, nxt, increasing, decreasing) is False:
+            return False
+    return True
+
+
+def calculate_safe_reports(reports: list[str]) -> int:
+    count = 0
+    for report in reports:
+        if report_is_safe(report):
+            count += 1
+    return count
+
+
+def part_1() -> None:
+    reports = read_input()
+    safe_report_count = calculate_safe_reports(reports)
+    console.print(Panel(f"[bold green]{safe_report_count=} [/]", title="[bold cyan]Day 1 - Part 1[/]", expand=False))
+
+
+def part_2() -> None:
+    pass
+
+
+def main() -> None:
+    part_1()
+    part_2()
+
+
+if __name__ == "__main__":
+    main()
