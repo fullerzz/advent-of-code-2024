bb489795352ed1835d91cd2400f01c5d02fe7645
Zach Fuller <zach.fuller1222@gmail.com>
2024-12-08 14:36:49 -0700

    wip: wrote logic to retrieve subset of valid updates

--- src/advent_of_code/day_5.py
+++ src/advent_of_code/day_5.py
@@ -22,7 +22,6 @@ def read_input() -> list[str]:
 def parse_rules(lines: list[str]) -> dict[int, list[int]]:
     rules: dict[int, list[int]] = {}
     for line in lines:
-        console.print(line)
         if line.find("|") == -1 or line == "\n":
             break
         parts = line.split("|")
@@ -34,9 +33,64 @@ def parse_rules(lines: list[str]) -> dict[int, list[int]]:
     return rules
 
 
+def parse_updates(lines: list[str]) -> list[list[int]]:
+    updates: list[list[int]] = []
+    second_section = False
+    for line in lines:
+        if not second_section and (line.find("|") == -1 or line == "\n"):
+            second_section = True
+            continue
+        if not second_section:
+            continue
+        update_pages = line.split(",")
+        update = [int(page) for page in update_pages]
+        updates.append(update)
+    return updates
+
+
+def prereq_met(update: list[int], prereq: int, val: int) -> bool:
+    prereq_index = -1
+    val_index = -1
+    for index, page in enumerate(update):
+        if page == val:
+            val_index = index
+        if page == prereq:
+            prereq_index = index
+    if prereq_index == -1:
+        return True
+    else:
+        return prereq_index < val_index
+
+
+def validate_update(update: list[int], rules: dict[int, list[int]]) -> bool:
+    for page in update:
+        page_rules = rules.get(page, [])
+        if page_rules != []:
+            console.print(f"Need to validate rules for {page=}")
+            console.print(f"{page_rules=}")
+            for rule in page_rules:
+                if not prereq_met(update, rule, page):
+                    return False
+    return True
+
+
+def validate_updates_against_rules(updates: list[list[int]], rules: dict[int, list[int]]) -> list[list[int]]:
+    """
+    Returns subset of updates that are valid
+    """
+    valid_updates: list[list[int]] = []
+    for update in updates:
+        if validate_update(update, rules) is True:
+            valid_updates.append(update)  # noqa: PERF401
+    return valid_updates
+
+
 def part_1(lines: list[str]) -> None:
     rules = parse_rules(lines)
-    console.print(Panel(f"[bold green]{rules=} [/]", title="[bold cyan]Day 1 - Part 1[/]", expand=False))
+    updates = parse_updates(lines)
+    valid_updates = validate_updates_against_rules(updates, rules)
+    console.print(Panel(f"[bold green]{len(updates)} [/]", title="[bold cyan]Day 5 - Part 1[/]", expand=False))
+    console.print(Panel(f"[bold green]{len(valid_updates)} [/]", title="[bold cyan]Day 5 - Part 1[/]", expand=False))
 
 
 def part_2() -> None:
